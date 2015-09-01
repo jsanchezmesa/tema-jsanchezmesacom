012560fc8d0ebc479fd0c74b8eaded81ecc4006f
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2015-09-01 19:02:59 +0200

    Eliminadas las entradas relacionadas en articulos

--- single.php
+++ single.php
@@ -18,38 +18,6 @@
 	<?php the_content(); ?>
 	</article>
 
-	<?php //muestra entradas relacionadas
-	$tags = wp_get_post_tags($post->ID);
-
-	if($tags){
-		$tag_ids = array();
-
-		foreach($tags as $individual_tag)
-			$tag_ids[] = $individual_tag->term_id;
-
-		$args = array(
-			'tag__in' => $tag_ids,
-			'post__not_in' => array($post->ID),
-			'showposts' => 3, //Número de posts relacionados a mostrar
-			'caller_get_posts' => 1
-		);
-
-		$my_query = new wp_query($args);
-
-		if($my_query->have_posts()){
-			echo '<h3>Artículos relacionados</h3><ul>';
-			while($my_query->have_posts()){
-				$my_query->the_post();
-				?>
-				<li><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permalink a <?php the_title_attribute(); ?>" ><?php the_title(); ?></a></li>
-				<?php
-			}
-			echo '</ul>';
-		}
-
-		wp_reset_query();
-	} ?>
-
 	<div class="etiquetas">
 		<?php the_tags(); ?>
 	</div>
